aa8a5632ff3529ee3a2e1cf542d1f15a55b5ada6
Van An Duong <andv2013@gmail.com>
2024-06-03 03:35:49 +1000

    Fix code

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.js
+module.exports = {
+    files: ["**/*.js"], 
+    languageOptions: {
+        ecmaVersion: 2021, 
+        sourceType: "module", 
+        globals: { 
+            window: "readonly",
+            document: "readonly",
+            process: "readonly"
+        }
+    },
+    plugins: {
+        sonarjs: {} 
+    },
+    rules: {
+        "indent": ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"]
+        
+    }
+};
