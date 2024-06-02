3ab145472b980776753f3f0e3091f361292ca556
Van An Duong <andv2013@gmail.com>
2024-06-03 04:01:27 +1000

    Fix code

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || "3000");
 
-app.set('port', port);
+app.set("port", port);
 
 function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number 
-        return port;
-    } return false;
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number 
+    return port;
+  } return false;
 }
 
 // Middlewares
-app.use(express.static('public_html'));
+app.use(express.static("public_html"));
 app.use(bodyParser.urlencoded({ extended: false })); 
 
 // Routes
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
 
-app.post('/feedback', (req, res) => {
-    let name = req.body.name;
-    let password = req.body.password;
-    let feedback = req.body.feedback;
-    let contact = req.body.contact;
+app.post("/feedback", (req, res) => {
+  let name = req.body.name;
+  let password = req.body.password;
+  let feedback = req.body.feedback;
+  let contact = req.body.contact;
     
-    const resp = `
+  const resp = `
     <html>
     <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
@@ -52,15 +52,15 @@ app.post('/feedback', (req, res) => {
         <p>Contact Method: <span style="color: blue;">${contact}</span></p>
         <p> Please click here <button onclick="window.location.href='/'" class="btn btn-primary">here</button> to return the feedback page.</p>
     `;
-    res.send(resp);
+  res.send(resp);
 });
 
 // Tell our application to listen to requests at port 3000 on the localhost
 app.listen(port, function () {
-    // When the application starts, print to the console that our app is
-    // running at http://localhost:3000 (where the port number is 3000 by
-    // default). Print another message indicating how to shut the server down.
-    console.log(`Web server running at: http://localhost:${port}`);
-    console.log("Type Ctrl+C to shut down the web server");
+  // When the application starts, print to the console that our app is
+  // running at http://localhost:3000 (where the port number is 3000 by
+  // default). Print another message indicating how to shut the server down.
+  console.log(`Web server running at: http://localhost:${port}`);
+  console.log("Type Ctrl+C to shut down the web server");
 });
 module.exports = app;
